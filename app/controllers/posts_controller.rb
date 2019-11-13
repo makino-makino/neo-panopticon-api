6c2ddbcacb5427fdbe19ca62d7ef89c38ca00ddf
makinoseiki <kino.ma.ms@gmail.com>
2019-11-13 10:17:21 +0900

    こっそりコメントアウト外したりした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_post, only: [:eval, :show, :update, :destroy]
 
   # GET /posts
