7062a01a7a0d49bd1425caa193fe8f2853d1363f
akakou <akakou571@gmail.com>
2020-02-28 15:55:52 +0900

    evaluationのリファクタリング

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,56 +1,22 @@
 class EvaluationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_evaluation, only: [:show, :update, :destroy]
-
-  # GET /evaluations
-  # GET /evaluations.json
-  def index
-    @evaluations = Evaluation.all
-  end
-
-  # GET /evaluations/1
-  # GET /evaluations/1.json
-  def show
-    params.require(:id)
-    @evaluation = Evaluation.find(params[:id])
-  end
+  before_action :set_evaluation, only: [:create]
 
   # POST /evaluations
   # POST /evaluations.json
   def create
-    if not (@evaluation = Evaluation.find_by(post_id:params[:post_id], user_id: current_user)).nil?
-      @evaluation.is_positive = params[:is_positive]
-    else
-      @evaluation = Evaluation.new(post_id: params[:post_id], is_positive: params[:is_positive], user: current_user)
-    end
+    is_positive = @evaluation.is_positive
 
-    if @evaluation.save
-      render :show, status: :created, location: @evaluation
-    else
-      render json: @evaluation.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /evaluations/1
-  # PATCH/PUT /evaluations/1.json
-  def update
-    if @evaluation.update(evaluation_params)
+    if @evaluation.update_attributes(is_positive: !is_positive)
       render :show, status: :ok, location: @evaluation
     else
       render json: @evaluation.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /evaluations/1
-  # DELETE /evaluations/1.json
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_evaluation
-      @evaluation = Evaluation.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def evaluation_params
-      params.fetch(:evaluation, {})
+      @evaluation = Evaluation.find_or_create_by(post_id:params[:post_id], user: current_user)
     end
 end
