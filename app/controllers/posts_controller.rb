b6deaff61f8d28f51e9527f37de286c3cd347f6d
akakou <akakou571@gmail.com>
2020-02-28 19:01:18 +0900

    PostsControllerをリファクタリングした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,29 +2,26 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:eval, :show, :update, :destroy]
 
+  DEFAULT_NUMBER = 20
+  DEFAULT_START_ID = 0
+
   # GET /posts
   # GET /posts.json
   def index
-    params.permit(:tl, :numbers, :start_created, :start_id, :user_id)
+    params.permit(:tl, :number, :start_id, :user_id)
 
-    numbers = params[:numbers].nil? ? 10 : params[:numbers].to_i
-    start_id = params[:start_id].nil? ? 0 : params[:start_id].to_i
-    start_created = params[:start_created].nil? ? 0 : params[:start_created].to_i
+    number = params[:number].nil? ? DEFAULT_NUMBER : params[:number].to_i
+    start_id = params[:start_id].nil? ? DEFAULT_START_ID : params[:start_id].to_i
 
-    # if params[:tl] == "local"
-    #   followings = Following.where(from: current_user)
-    #   users = User.where(user: followings)
-    # else
-      users = User.all
-    # end
+    @posts = Post.all.where(`start_id <= ?`, start_id)
+    if params[:type] == "local"
+      @posts = Post.local(current_user)
+    elsif params[:type] == 'user'
+      @posts = Posts.where(user_id: user_id)
+    end
 
-    @posts = Post.tl(params[:tl], users, numbers, start_id, start_created)
-  end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-    @score = Evaluation.eval_post(@post)
+    @posts = @posts.last(number * 2)
+    @posts = Post.choose(@posts)
   end
 
   # POST /posts
@@ -42,33 +39,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def eval
-    params.require(:score)
-    params.permit(:score)
-    score = params[:score]
-
-    # TODO: Validation
-
-    @post = Post.find(params['id'])
-
-    # validate
-    @evaluation = Evaluation.find_by(post_id: @post.id, user_id: current_user.id)
-    if not (@evaluation).nil? 
-      render :eval
-    else
-      @evaluation = Evaluation.new(post: @post, user:current_user, score: score)
-
-      if @evaluation.save
-        render :eval
-      else
-        render json: @evaluation.errors, status: :unprocessable_entity
-      end
-    end
-
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
@@ -76,7 +47,7 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:content)
-      params.permit(:content) #, :type, :souce_id)
+      params.permit(:content)
     end
 
 end
