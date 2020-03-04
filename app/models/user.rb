5ce504becbf08873325bfbe8f46cda08444ba8dd
akakou <akakou571@gmail.com>
2020-03-04 23:18:48 +0900

    メールをとばすようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable # , :trackable
+         :recoverable, :rememberable, :validatable, :confirmable# , :trackable
   include DeviseTokenAuth::Concerns::User
   validates :name, uniqueness: true
   validates :email, uniqueness: true
