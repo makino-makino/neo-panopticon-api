fc5b39c9ccdb311241f5d763209a0864df56545e
akakou <akakou571@gmail.com>
2020-03-04 23:18:48 +0900

    メールをとばすようにした

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -52,4 +52,5 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you can
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   # config.enable_standard_devise_support = false
+  config.default_confirm_success_url = 'confirmation'
 end
