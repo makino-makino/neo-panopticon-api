5bc45ce1f6a2b35a57c71ba57ea4d54286969654
akakou <akakou571@gmail.com>
2020-03-05 20:38:53 +0900

    posgresに移行した

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@
 rm ./tmp/pids/server.pid
 #export $(cat .env | grep -v ^# | xargs);
 
-rails db:create
-rails db:migrate RAILS_ENV=development
+#rails db:create
+#rails db:migrate RAILS_ENV=development
+#heroku pg:reset neo-panopticon
 
 bundle exec rails s -p $PORT -b '0.0.0.0'
 
