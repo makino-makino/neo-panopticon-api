3033001ffa5ab7215f5277a0d4504e0877124f98
akakou <akakou571@gmail.com>
2020-03-05 19:09:00 +0900

    railsとposgresを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_02_29_022037) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "evaluations", force: :cascade do |t|
-    t.integer "post_id"
+    t.bigint "post_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,8 +25,8 @@ ActiveRecord::Schema.define(version: 2020_02_29_022037) do
   end
 
   create_table "followings", force: :cascade do |t|
-    t.integer "from_id", null: false
-    t.integer "to_id", null: false
+    t.bigint "from_id", null: false
+    t.bigint "to_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["from_id"], name: "index_followings_on_from_id"
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_022037) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -70,4 +73,8 @@ ActiveRecord::Schema.define(version: 2020_02_29_022037) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "evaluations", "posts"
+  add_foreign_key "followings", "users", column: "from_id"
+  add_foreign_key "followings", "users", column: "to_id"
+  add_foreign_key "posts", "users"
 end
